6fa1db3e953634208a4de546ef5d9cf8d5fc29c1
PellissardKevin <kevin.pellissard@gmail.com>
2024-05-14 13:17:03 +0200

    Update camera controller script for task 1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,16 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
-    // Start is called before the first frame update
+
     void Start()
     {
+        // Calculate and store the offset value by getting the distance between the player's position and the camera's position.
         offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = player.transform.position + offset;
     }
 }
