b22ec45b3c0088a0d19060355ae9555458fdb2e0
PellissardKevin <kevin.pellissard@gmail.com>
2024-05-14 11:52:14 +0200

    Task 1 Player movement with script and rigidbody

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1000;
+
+    private Rigidbody playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        playerBody.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+    }
+}
